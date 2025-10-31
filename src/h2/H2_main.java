ccbd86541dc83d2f565f7cf98af08fec7bf1023b
Viktoriia <viktoriiapohromska@gmail.com>
2025-10-31 14:41:20 +0100

    Initial commit

---
+++ src/h2/H2_main.java
@@ -0,0 +1,29 @@
+package h2;
+
+public class H2_main {
+	public static void main(String[] args) {
+		int i = 64;
+		int j = 17;
+		int k = 22;
+		int min;
+		int max;
+
+		min = i;
+		if (j < min) {
+			min = j;
+		}
+		if (k < min) {
+			min = k;
+		}
+
+		max = i;
+		if (j > max) {
+			max = j;
+		}
+		if (k > max) {
+			max = k;
+		}
+		System.out.println("min: " + min);
+		System.out.println("max: " + max);
+	}
+}
---
+++ src/h3/H3_main.java
@@ -0,0 +1,26 @@
+package h3;
+
+public class H3_main {
+	public static void main(String[] args) {
+		int i = 164;
+		int j = 117;
+		int k = -10;
+
+		if (i > j) {
+			k = 1;
+
+			if (i > 200) {
+				k = 2;
+
+				if (j > 100) {
+					k = 3;
+				}
+			}
+		} else if (i <= 200) {
+			if (j <= 100) {
+				k = 4;
+			}
+		}
+		System.out.println("k =  " + k);
+	}
+}
